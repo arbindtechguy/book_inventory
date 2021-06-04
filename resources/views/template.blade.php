1e9c3c7ebf1c5940e38488513f163482a17a6f4e
Gcomparison <arbindtechguy@gmail.com>
2021-06-05 00:34:20 +0900

    direct url correction

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -19,20 +19,21 @@ $keywords = !empty($keywords) ? $keywords : '';
 
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+@if ($title)
     <title>{{ $title }}</title>
+@endif
     <meta name="keyword" content="{{ $keywords }}" />
     @if ($desc)
     <meta name="description" content="{{ $desc }}" />
     @endif
     <link rel="icon" type="image/png" sizes="32x32" href="/favicon.ico">
-    <link rel="stylesheet"
-        href="{{ asset('assets/css/dashboard.css') }}?{{ File::lastModified('assets/css/dashboard.css') }}">
-    <link rel="stylesheet" href="{{env('APP_URL')}}/vendor/laravel-admin/AdminLTE/dist/css/AdminLTE.min.css">
+    <link rel="stylesheet" href="{{ asset('assets/css/dashboard.css') }}?{{ File::lastModified('assets/css/dashboard.css') }}">
+    <link rel="stylesheet" href="{{ env('APP_URL') }}/vendor/laravel-admin/AdminLTE/dist/css/AdminLTE.min.css">
     <script src="{{env('APP_URL')}}/vendor/laravel-admin/AdminLTE/plugins/jQuery/jQuery-2.1.4.min.js"></script>
     <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"
         integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
 
-    <link rel="stylesheet" href="{{env('APP_URL')}}/vendor/laravel-admin/AdminLTE/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="{{ env('APP_URL') }}/vendor/laravel-admin/AdminLTE/bootstrap/css/bootstrap.min.css">
 
 
 </head>
@@ -47,9 +48,6 @@ $keywords = !empty($keywords) ? $keywords : '';
     {{-- scripts --}}
 
     <script src="{{env('APP_URL')}}/vendor/laravel-admin/AdminLTE/bootstrap/js/bootstrap.min.js"></script>
-
-
-
 </body>
 
 </html>
